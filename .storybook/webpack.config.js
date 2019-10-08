549387ab2ca7a7221396e42a1e27145fb81e6e71
David Li <dawei.li@gmx.net>
2019-10-08 11:33:47 +0200

    skipLibCheck für storybook

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -8,11 +8,15 @@ module.exports = ({ config }) => {
         '@babel/preset-react',
         '@babel/preset-typescript'
       ],
-      plugins: [  
-        '@babel/plugin-proposal-class-properties'
-      ]
+      plugins: ['@babel/plugin-proposal-class-properties']
     }
   })
+  // avoid possible ts errors we are not interested in
+  for (const plugin of config.plugins) {
+    if (plugin.constructor.name === 'ForkTsCheckerWebpackPlugin') {
+      plugin.compilerOptions.skipLibCheck = true
+    }
+  }
   config.resolve.extensions.push('.ts', '.tsx')
   config.devServer = {
     stats: 'errors-only'
